2c1e31eede9baf20c014caa6ee4faf6c87ed8a5e
sundarshahi <shahithakurisundar@gmail.com>
2020-01-31 23:12:40 +0545

    feat: Add sidebar on left
    add sidebar on left to display health realted issues and trending topic.

---
+++ src/components/SideNav/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const SideNav = () => (
+  <ul className="list-group">
+    <li className="list-group-item">
+      Virus
+    </li>
+    <li className="list-group-item">
+      Infection
+    </li>
+    <li className="list-group-item">
+      Type-1-diabetes
+    </li>
+    <li className="list-group-item">
+      Cancer
+    </li>
+  </ul>
+);
+
+export default SideNav;
