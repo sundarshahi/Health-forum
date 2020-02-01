42cea52c22c81f85e3a69dc8f03f71ef1777bd89
sundarshahi <shahithakurisundar@gmail.com>
2020-02-01 10:01:07 +0545

    Create store

---
+++ src/redux/index.js
@@ -0,0 +1,19 @@
+import {createStore,compose,applyMiddleware} from 'redux'
+import thunk from 'redux-thunk';
+
+const reducer=(state = {},action) =>{
+    return state
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store=createStore(
+    
+    reducer,
+    composeEnhancers(
+        applyMiddleware(thunk)
+    )
+    
+    )
+
+export default store
