c18b01cc8f4271e9c47467f5b4e2c58827b0b20f
sundarshahi <shahithakurisundar@gmail.com>
2020-02-03 21:16:18 +0545

    Create reducer for single thread

---
+++ src/redux/reducers/thread.js
@@ -0,0 +1,36 @@
+  
+import { GET_REPLIES, GET_REPLIES_LOADED } from '../actions/replies'
+import { GET_THREAD, GET_THREAD_LOADED, GET_THREAD_LOADING } from '../actions/threads'
+
+const threadReducer = (state={loading:true,loadingReplies: true, replies:{data:[]}},action) =>{
+    switch (action.type) {
+        case GET_THREAD:
+          return {
+            ...state,
+            data: action.payload
+          }
+        case GET_THREAD_LOADING:
+          return {
+            ...state,
+            loading: true
+          }
+        case GET_THREAD_LOADED:
+          return {
+            ...state,
+            loading: false
+          }
+        case GET_REPLIES:
+          return {
+            ...state,
+            replies: action.payload
+          }
+        case GET_REPLIES_LOADED:
+          return {
+            ...state,
+            loadingReplies: false
+          }
+        default:
+          return state;
+      }
+}
+export default threadReducer
